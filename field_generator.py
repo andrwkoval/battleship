677cb9038dd9bfa435d31a3ec6c3de5f095411de
Andrii Koval <drone.kov@gmail.com>
2018-03-10 14:33:17 +0200

    ship size

--- field_generator.py
+++ field_generator.py
@@ -2,9 +2,9 @@ import string
 
 
 def read_field(filename):
-    with open(filename, "r", encoding="utf-8", errors="ignore") as f:
+    with open(filename, "r", encoding="utf-8", errors="ignore") as file_ships:
         field = [[0] * 10 for i in range(10)]
-        data = f.readlines()
+        data = file_ships.readlines()
         for i in range(10):
             for j in range(len(data[i][:10])):
                 if data[i][j] == "*":
@@ -16,23 +16,34 @@ def read_field(filename):
 
 def has_ship(field, coords):
     lets = list(string.ascii_uppercase[:10])
-    if field[coords[1] - 1][lets.index(coords[0])] == 1:
+    if field[coords[1] - 1][lets.index(coords[0].upper())] == 1:
         return True
     return False
 
 
 def ship_size(field, coords):
     lets = list(string.ascii_uppercase[:10])
-    pass
 
+    if not has_ship(field, coords):
+        return (0, 0)
 
+    ver, hor = coords[1] - 1, lets.index(coords[0].upper())
+    horizontal, vertical = 1, 1
 
+    step = 1
+    while ver - step > 0 and field[ver - step][hor] == 1:
+        vertical, step = vertical + 1, step + 1
 
+    step = 1
+    while ver + step < 10 and field[ver + step][hor] == 1:
+        vertical, step = vertical + 1, step + 1
 
+    step = 1
+    while hor - step > 0 and field[ver][hor - step] == 1:
+        horizontal, step = horizontal + 1, step + 1
 
-a = read_field("field.txt")
+    step = 1
+    while hor + step < 10 and field[ver][hor + step] == 1:
+        horizontal, step = horizontal + 1, step + 1
 
-for i in a:
-    print(i)
-
-print(has_ship(a, ("D", 1)))
+    return (horizontal, vertical)
