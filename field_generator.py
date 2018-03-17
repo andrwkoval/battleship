7d92df754ef28a18a4bae96fe2e0d576e6784642
Andrii Koval <drone.kov@gmail.com>
2018-03-17 22:41:53 +0200

    final commit

--- field_generator.py
+++ field_generator.py
@@ -45,7 +45,7 @@ def ship_size(field, coords):
 
 def is_valid(field):
     lets = list(alpha[:10])
-    ships = [4]*4 + [3]*6 + [2]*6 + [1]*4
+    ships = [4] * 4 + [3] * 6 + [2] * 6 + [1] * 4
 
     if len([1 for i in field for j in i if j == 1]) != 20:
         return False
@@ -56,9 +56,9 @@ def is_valid(field):
 
     for i in range(len(field)):
         for j in range(len(field[i])):
-            if has_ship(field, (lets[j], i+1)) and \
-                    valid_location(field, (lets[j], i+1)):
-                a = ship_size(field, (lets[j], i+1))
+            if has_ship(field, (lets[j], i + 1)) and \
+                    valid_location(field, (lets[j], i + 1)):
+                a = ship_size(field, (lets[j], i + 1))
                 try:
                     if a[0] >= a[1] and a[1] == 1:
                         ships.remove(a[0])
@@ -74,23 +74,23 @@ def is_valid(field):
 def valid_location(data, coords):
     lets = list(alpha[:10])
     y, x = coords[1] - 1, lets.index(coords[0].upper())
-    if (0<y<9 and 0<x<9) and \
-        (data[y - 1][x - 1] or data[y - 1][x + 1] or
-            data[y + 1][x - 1] or data[y + 1][x + 1]):
+    if (0 < y < 9 and 0 < x < 9) and \
+            (data[y - 1][x - 1] or data[y - 1][x + 1] or
+             data[y + 1][x - 1] or data[y + 1][x + 1]):
         return False
     return True
 
 
 def generate_field():
-    field = [[0]*10 for i in range(10)]
-    ships = [1]*4 + [2]*3 + [3]*2 + [4]
+    field = [[0] * 10 for i in range(10)]
+    ships = [1] * 4 + [2] * 3 + [3] * 2 + [4]
     while ships:
         ver, hor = rd.randint(ships[-1] - 1, 9), rd.randint(ships[-1] - 1, 9)
-        if rd.randint(0,1):
+        if rd.randint(0, 1):
             for i in range(ships[-1]):
                 pointer = False
                 if not has_ship(field, (alpha[hor], ver + 1)) and \
-                    valid_location(field, (alpha[hor], ver + 1)):
+                        valid_location(field, (alpha[hor], ver + 1)):
                     pointer = True
                 ver -= 1
                 if not pointer:
@@ -105,7 +105,7 @@ def generate_field():
             for i in range(ships[-1]):
                 pointer = False
                 if not has_ship(field, (alpha[hor], ver + 1)) and \
-                    valid_location(field, (alpha[hor], ver + 1)):
+                        valid_location(field, (alpha[hor], ver + 1)):
                     pointer = True
                 hor -= 1
                 if not pointer:
@@ -126,7 +126,9 @@ def field_to_str(field):
     str_field = "    " + "  ".join(list(alpha[:10])) + "\n"
     for i, j in enumerate(field):
         if i < 9:
-            str_field += str(i + 1) + "   " + "  ".join(map(str, j)) + "\n"
+            str_field += str(i + 1) + " | " + "| ".join(map(str, j))\
+                         + "|" + "\n"
         else:
-            str_field += str(i + 1) + "  " + "  ".join(map(str, j)) + "\n"
+            str_field += str(i + 1) + "| " + "| ".join(map(str, j))\
+                         + "|" + "\n"
     return str_field
--- game.py
+++ game.py
@@ -1,2 +1,153 @@
+from field_generator import *
+import copy
+
+
 class Ship:
+    def __init__(self, bow, horizontal, length):
+        self.bow = bow
+        self.horizontal = horizontal
+        self.__length = length
+        self.__hit = [False] * self.__length
+
+    def shoot_at(self, tup):
+        if self.horizontal:
+            for i in range(self.__length):
+                if (self.bow[1] + i, self.bow[0]) == tup:
+                    self.__hit[i] = True
+        else:
+            for i in range(self.__length):
+                if (self.bow[1], self.bow[0] + i) == tup:
+                    self.__hit[i] = True
+
+    def __repr__(self):
+        return "*"
+
+
+class Field:
     def __init__(self):
+        self.__ships = generate_field()
+        for i in range(len(self.__ships)):
+            for j in range(len(self.__ships[i])):
+                if self.__ships[i][j] == 1:
+                    l = ship_size(self.__ships, (chr(j + 65), i + 1))
+                    if l[1] == 1:
+                        ship = Ship((i, j), True, l[0])
+                        for k in range(l[0]):
+                            self.__ships[i][j + k] = ship
+                    elif l[0] == 1:
+                        ship = Ship((i, j), True, l[1])
+                        for k in range(l[1]):
+                            self.__ships[i + k][j] = ship
+        self.ships = self.__ships
+
+    def shoot_at(self, tup):
+        if isinstance(self.__ships[tup[0]][tup[1]], Ship):
+            self.__ships[tup[0]][tup[1]].shoot_at(tup)
+            self.__ships[tup[0]][tup[1]] = "X"
+            return True
+        elif self.__ships[tup[0]][tup[1]] == "X":
+            return True
+        else:
+            self.__ships[tup[0]][tup[1]] = "O"
+            return False
+
+    def field_without_ships(self):
+        field = copy.deepcopy(self.__ships)
+        for i in range(len(field)):
+            for j in range(len(field)):
+                if field[i][j] == "O" or field[i][j] == "X":
+                    pass
+                else:
+                    field[i][j] = " "
+        return field_to_str(field)
+
+    def field_with_ships(self):
+        field = copy.deepcopy(self.__ships)
+        for i in range(len(field)):
+            for j in range(len(field[i])):
+                if field[i][j] == 0:
+                    field[i][j] = " "
+        return field_to_str(field)
+
+
+class Player:
+    def __init__(self, name):
+        self.__name = name
+
+    def read_position(self):
+        pos = input().split(" ")
+        try:
+            ver, hor = int(pos[1]) - 1, list(alpha[:10]).index(pos[0].upper())
+            assert 0 <= ver < 10 and 0 <= hor < 10
+        except (AssertionError, ValueError, IndexError):
+            print("Wrong coordinates, enter again: ")
+            return self.read_position()
+        return ver, hor
+
+
+class Game:
+    def __init__(self, current_player=0):
+        self.__field = [Field(), Field()]
+        self.__players = [Player(input("Player 1, enter your name: ")),
+                          Player(input("Player 2, enter your name: "))]
+        self.__current_player = current_player
+
+    def read_position(self):
+        return self.__players[self.__current_player].read_position()
+
+    def field_without_ships(self, index):
+        return self.__field[index].field_without_ships()
+
+    def field_with_ships(self, index):
+        return self.__field[index].field_with_ships()
+
+    def winner(self, player):
+        if self.__field[player - 1].field_with_ships().count("X") == 20:
+            return True
+        return False
+
+    def start_game(self):
+        print("Game Battleship")
+        while True:
+            self.__current_player = 0
+            print("Player {}, it`s your turn now!\n"
+                  "Enter coordinates to "
+                  "shoot: ".format(str(self.__current_player + 1)))
+            coords = self.read_position()
+            shoot = self.__field[self.__current_player].shoot_at(coords)
+            while shoot:
+                print("Nice, shoot again: ")
+                print(
+                    self.__field[self.__current_player].field_without_ships())
+                coords = self.read_position()
+                shoot = self.__field[self.__current_player].shoot_at(coords)
+            else:
+                print("Uhhh, you missed")
+            print(self.__field[self.__current_player].field_without_ships())
+            if self.winner(self.__current_player):
+                print("Player {} WON!!! "
+                      "Congratulations!".format(self.__current_player))
+            self.__current_player += 1
+            print("Player {}, it`s your turn now!\n"
+                  "Enter coordinates to "
+                  "shoot: ".format(str(self.__current_player+1)))
+            coords = self.read_position()
+            shoot = self.__field[self.__current_player].shoot_at(coords)
+            while shoot:
+                print("Nice, shoot again: ")
+                print(
+                    self.__field[self.__current_player].field_without_ships())
+                coords = self.read_position()
+                shoot = self.__field[self.__current_player].shoot_at(coords)
+            else:
+                print("Uhhh, you missed")
+            print(self.__field[self.__current_player].field_without_ships())
+            if self.winner(self.__current_player):
+                print("Player {} WON!!! "
+                      "Congratulations!".format(self.__current_player))
+                exit()
+
+
+
+game = Game()
+game.start_game()
